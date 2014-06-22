d67a40531e26d5211324edd918a965577c531d9d
Sam Galison <samface@gmail.com>
2014-06-21 20:20:07 -0400

    geolocation fix, hopefully

--- wordpress/wp-content/plugins/geolocation-plus/geolocation.php
+++ wordpress/wp-content/plugins/geolocation-plus/geolocation.php
@@ -520,9 +520,9 @@ function reverse_geocode($latitude, $longitude) {
 
     if ($iserr != 1) {
             $json = json_decode($result['body']);
-            foreach ($json->results as $result)
+            foreach ($json->results as $single_result)
                 {
-                    foreach($result->address_components as $addressPart) {
+                    foreach($single_result->address_components as $addressPart) {
                         if((in_array('locality', $addressPart->types)) && (in_array('political', $addressPart->types)))
                             $city = $addressPart->long_name;
                         else if((in_array('administrative_area_level_1', $addressPart->types)) && (in_array('political', $addressPart->types)))
